a69e6f504d985769940ef0c165a099ba2a416f42
oumartom <oumartom45@gmail.com>
2025-08-21 12:02:05 +0100

    Prod settings: dj-database-url, whitenoise, env vars

--- techEdgeWebSite/settings.py
+++ techEdgeWebSite/settings.py
@@ -81,10 +81,11 @@ WSGI_APPLICATION = 'techEdgeWebSite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 DATABASES = {
-    "default": dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600,
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
     )
 }
 # DATABASES = {
