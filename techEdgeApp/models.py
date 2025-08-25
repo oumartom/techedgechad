db35a4a96b17ad1a4f084e847097bab30ab802bc
oumartom <oumartom45@gmail.com>
2025-08-25 16:32:58 +0100

    config de cloudinary et nettoyage

--- techEdgeApp/models.py
+++ techEdgeApp/models.py
@@ -1,28 +1,30 @@
 from django.db import models
 from django.utils.text import slugify
 from django.urls import reverse
-from cloudinary.models import CloudinaryField
+from django.conf import settings
 
-# class ProjectCategory(models.Model):
-#     name = models.CharField("Nom de la catégorie", max_length=100)
-#     slug = models.SlugField("Slug", unique=True)
-#     description = models.TextField("Description", blank=True)
-#     icon = models.CharField("Icône FontAwesome", max_length=50, default="fas fa-folder")
-#     display_order = models.PositiveIntegerField("Ordre d'affichage", default=0)
-#     is_active = models.BooleanField("Active", default=True)
+# Déterminer le type de champ image à utiliser
+def get_image_field():
+    """Retourne le champ approprié selon la configuration"""
+    # Vérifier si Cloudinary est configuré dans les settings
+    use_cloudinary = getattr(settings, 'USE_CLOUDINARY', False)
+    
+    if use_cloudinary:
+        from cloudinary.models import CloudinaryField
+        return CloudinaryField('image', folder='services/', null=True, blank=True)
+    else:
+        return models.ImageField("Image", upload_to='services/', null=True, blank=True)
 
-#     class Meta:
-#         verbose_name = "Catégorie de projet"
-#         verbose_name_plural = "Catégories de projet"
-#         ordering = ['display_order', 'name']
-
-#     def __str__(self):
-#         return self.name
-
-#     def save(self, *args, **kwargs):
-#         if not self.slug:
-#             self.slug = slugify(self.name)
-#         super().save(*args, **kwargs)
+def get_photo_field(folder_name, verbose_name):
+    """Retourne le champ approprié selon la configuration"""
+    # Vérifier si Cloudinary est configuré dans les settings
+    use_cloudinary = getattr(settings, 'USE_CLOUDINARY', False)
+    
+    if use_cloudinary:
+        from cloudinary.models import CloudinaryField
+        return CloudinaryField(verbose_name, folder=folder_name, null=True, blank=True)
+    else:
+        return models.ImageField(verbose_name, upload_to=folder_name, null=True, blank=True)
 
 class Service(models.Model):
     title = models.CharField("Titre", max_length=100)
@@ -30,8 +32,7 @@ class Service(models.Model):
     long_description = models.TextField("Description longue", blank=True)
     icon_class = models.CharField("Classe d'icône", max_length=50, blank=True, 
                                  help_text="Ex: fa fa-laptop-code")
-    image = CloudinaryField('Image', folder='services/', null=True, blank=True)
-    #image = models.ImageField("Image", upload_to='services/')
+    image = get_image_field()
     slug = models.SlugField(unique=True, blank=True)
     is_featured = models.BooleanField("Mettre en avant", default=False,
                                     help_text="Cocher pour afficher ce service en page d'accueil")
@@ -40,7 +41,6 @@ class Service(models.Model):
     class Meta:
         verbose_name = "Service"
         verbose_name_plural = "Services"
-        ordering = ['display_order', 'title']
         ordering = ['-is_featured', 'display_order']
     
     def save(self, *args, **kwargs):
@@ -67,9 +67,7 @@ class TeamMember(models.Model):
     position_type = models.CharField("Type de poste", max_length=20, 
                                     choices=POSITION_CHOICES, default='technical')
     bio = models.TextField("Biographie", blank=True)
-    # image = models.ImageField("Photo", upload_to='team/')
-
-    image = CloudinaryField('Photo', folder='team/', null=True, blank=True)
+    image = get_photo_field('team/', 'Photo')
     facebook = models.URLField("Facebook", blank=True)
     twitter = models.URLField("Twitter", blank=True)
     linkedin = models.URLField("LinkedIn", blank=True)
@@ -98,15 +96,14 @@ class Project(models.Model):
     title = models.CharField("Titre", max_length=100)
     short_description = models.CharField("Description courte", max_length=200)
     long_description = models.TextField("Description détaillée")
-    # image = models.ImageField("Image principale", upload_to='projects/')
-    image = CloudinaryField('Image principale', folder='projects/',null=True, blank=True)
+    image = get_photo_field('projects/', 'Image principale')
     category = models.CharField(
-    "Catégorie", 
-    max_length=20, 
-    choices=PROJECT_CATEGORIES, 
-    blank=True, 
-    null=True
-)
+        "Catégorie", 
+        max_length=20, 
+        choices=PROJECT_CATEGORIES, 
+        blank=True, 
+        null=True
+    )
     client_name = models.CharField("Client", max_length=100, blank=True)
     project_date = models.DateField("Date du projet", blank=True, null=True)
     project_url = models.URLField("URL du projet", blank=True)
@@ -120,6 +117,7 @@ class Project(models.Model):
     
     def __str__(self):
         return self.title
+    
     def get_absolute_url(self):
         return reverse('project_detail', args=[str(self.pk)])
 
@@ -128,8 +126,7 @@ class Testimonial(models.Model):
     company = models.CharField("Entreprise", max_length=100, blank=True)
     profession = models.CharField("Profession", max_length=100)
     content = models.TextField("Témoignage")
-    # image = models.ImageField("Photo", upload_to='testimonials/', blank=True)
-    image = CloudinaryField('Photo', folder='testimonials/',null=True, blank=True)
+    image = get_photo_field('testimonials/', 'Photo')
     rating = models.PositiveSmallIntegerField("Note (1-5)", default=5)
     is_featured = models.BooleanField("En vedette", default=False)
     display_order = models.PositiveIntegerField("Ordre d'affichage", default=0)
@@ -169,10 +166,189 @@ class Fact(models.Model):
     
     def get_icon_class(self):
         return f"fa fa-{self.icon}"
-    
+
 class Subscriber(models.Model):
     email = models.EmailField(unique=True)
     date_subscribed = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.email
+
+# from django.db import models
+# from django.utils.text import slugify
+# from django.urls import reverse
+# from cloudinary.models import CloudinaryField
+
+# # class ProjectCategory(models.Model):
+# #     name = models.CharField("Nom de la catégorie", max_length=100)
+# #     slug = models.SlugField("Slug", unique=True)
+# #     description = models.TextField("Description", blank=True)
+# #     icon = models.CharField("Icône FontAwesome", max_length=50, default="fas fa-folder")
+# #     display_order = models.PositiveIntegerField("Ordre d'affichage", default=0)
+# #     is_active = models.BooleanField("Active", default=True)
+
+# #     class Meta:
+# #         verbose_name = "Catégorie de projet"
+# #         verbose_name_plural = "Catégories de projet"
+# #         ordering = ['display_order', 'name']
+
+# #     def __str__(self):
+# #         return self.name
+
+# #     def save(self, *args, **kwargs):
+# #         if not self.slug:
+# #             self.slug = slugify(self.name)
+# #         super().save(*args, **kwargs)
+
+# class Service(models.Model):
+#     title = models.CharField("Titre", max_length=100)
+#     short_description = models.CharField("Description courte", max_length=200, blank=True)
+#     long_description = models.TextField("Description longue", blank=True)
+#     icon_class = models.CharField("Classe d'icône", max_length=50, blank=True, 
+#                                  help_text="Ex: fa fa-laptop-code")
+#     image = CloudinaryField('Image', folder='services/', null=True, blank=True)
+#     #image = models.ImageField("Image", upload_to='services/')
+#     slug = models.SlugField(unique=True, blank=True)
+#     is_featured = models.BooleanField("Mettre en avant", default=False,
+#                                     help_text="Cocher pour afficher ce service en page d'accueil")
+#     display_order = models.PositiveIntegerField("Ordre d'affichage", default=0)
+    
+#     class Meta:
+#         verbose_name = "Service"
+#         verbose_name_plural = "Services"
+#         ordering = ['display_order', 'title']
+#         ordering = ['-is_featured', 'display_order']
+    
+#     def save(self, *args, **kwargs):
+#         if not self.slug:
+#             self.slug = slugify(self.title)
+#         super().save(*args, **kwargs)
+    
+#     def __str__(self):
+#         return self.title
+    
+#     def get_absolute_url(self):
+#         return reverse('service_detail', kwargs={'slug': self.slug})
+
+# class TeamMember(models.Model):
+#     POSITION_CHOICES = [
+#         ('management', 'Management'),
+#         ('technical', 'Technique'),
+#         ('design', 'Design'),
+#         ('training', 'Formation'),
+#     ]
+    
+#     name = models.CharField("Nom complet", max_length=100)
+#     position = models.CharField("Poste", max_length=100)
+#     position_type = models.CharField("Type de poste", max_length=20, 
+#                                     choices=POSITION_CHOICES, default='technical')
+#     bio = models.TextField("Biographie", blank=True)
+#     # image = models.ImageField("Photo", upload_to='team/')
+
+#     image = CloudinaryField('Photo', folder='team/', null=True, blank=True)
+#     facebook = models.URLField("Facebook", blank=True)
+#     twitter = models.URLField("Twitter", blank=True)
+#     linkedin = models.URLField("LinkedIn", blank=True)
+#     instagram = models.URLField("Instagram", blank=True)
+#     email = models.EmailField("Email", blank=True)
+#     phone = models.CharField("Téléphone", max_length=20, blank=True)
+#     display_order = models.PositiveIntegerField("Ordre d'affichage", default=0)
+#     is_active = models.BooleanField("Actif", default=True)
+    
+#     class Meta:
+#         verbose_name = "Membre d'équipe"
+#         verbose_name_plural = "Membres d'équipe"
+#         ordering = ['display_order', 'name']
+    
+#     def __str__(self):
+#         return f"{self.name} - {self.position}"
+
+# class Project(models.Model):
+#     PROJECT_CATEGORIES = [
+#         ('web', 'Développement Web'),
+#         ('mobile', 'Application Mobile'),
+#         ('system', 'Système de Gestion'),
+#         ('training', 'Formation'),
+#         ('consulting', 'Consulting'),
+#     ]
+#     title = models.CharField("Titre", max_length=100)
+#     short_description = models.CharField("Description courte", max_length=200)
+#     long_description = models.TextField("Description détaillée")
+#     # image = models.ImageField("Image principale", upload_to='projects/')
+#     image = CloudinaryField('Image principale', folder='projects/',null=True, blank=True)
+#     category = models.CharField(
+#     "Catégorie", 
+#     max_length=20, 
+#     choices=PROJECT_CATEGORIES, 
+#     blank=True, 
+#     null=True
+# )
+#     client_name = models.CharField("Client", max_length=100, blank=True)
+#     project_date = models.DateField("Date du projet", blank=True, null=True)
+#     project_url = models.URLField("URL du projet", blank=True)
+#     is_featured = models.BooleanField("En vedette", default=False)
+#     display_order = models.PositiveIntegerField("Ordre d'affichage", default=0)
+    
+#     class Meta:
+#         verbose_name = "Projet"
+#         verbose_name_plural = "Projets"
+#         ordering = ['-is_featured', 'display_order']
+    
+#     def __str__(self):
+#         return self.title
+#     def get_absolute_url(self):
+#         return reverse('project_detail', args=[str(self.pk)])
+
+# class Testimonial(models.Model):
+#     client_name = models.CharField("Nom du client", max_length=100)
+#     company = models.CharField("Entreprise", max_length=100, blank=True)
+#     profession = models.CharField("Profession", max_length=100)
+#     content = models.TextField("Témoignage")
+#     # image = models.ImageField("Photo", upload_to='testimonials/', blank=True)
+#     image = CloudinaryField('Photo', folder='testimonials/',null=True, blank=True)
+#     rating = models.PositiveSmallIntegerField("Note (1-5)", default=5)
+#     is_featured = models.BooleanField("En vedette", default=False)
+#     display_order = models.PositiveIntegerField("Ordre d'affichage", default=0)
+#     created_at = models.DateTimeField("Date de création", auto_now_add=True)
+    
+#     class Meta:
+#         verbose_name = "Témoignage"
+#         verbose_name_plural = "Témoignages"
+#         ordering = ['-is_featured', 'display_order']
+    
+#     def __str__(self):
+#         return f"{self.client_name} - {self.profession}"
+
+# class Fact(models.Model):
+#     ICON_CHOICES = [
+#         ('certificate', 'Certificat'),
+#         ('users-cog', 'Utilisateurs'),
+#         ('users', 'Clients'),
+#         ('check', 'Validation'),
+#         ('project', 'Projet'),
+#     ]
+    
+#     title = models.CharField("Titre", max_length=100)
+#     value = models.IntegerField("Valeur")
+#     icon = models.CharField("Icône", max_length=20, choices=ICON_CHOICES, default='certificate')
+#     suffix = models.CharField("Suffixe", max_length=5, blank=True, default='+',
+#                              help_text="Ex: +, %, etc.")
+#     display_order = models.PositiveIntegerField("Ordre d'affichage", default=0)
+    
+#     class Meta:
+#         verbose_name = "Fait marquant"
+#         verbose_name_plural = "Faits marquants"
+#         ordering = ['display_order']
+    
+#     def __str__(self):
+#         return self.title
+    
+#     def get_icon_class(self):
+#         return f"fa fa-{self.icon}"
+    
+# class Subscriber(models.Model):
+#     email = models.EmailField(unique=True)
+#     date_subscribed = models.DateTimeField(auto_now_add=True)
+
+#     def __str__(self):
+#         return self.email
--- techEdgeWebSite/settings.py
+++ techEdgeWebSite/settings.py
@@ -13,9 +13,7 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 import dj_database_url
-import cloudinary
-import cloudinary.uploader
-import cloudinary.api
+
 # Charger .env
 load_dotenv()
 
@@ -23,6 +21,42 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-change-me")
 DEBUG = os.environ.get("DEBUG", "1") == "1"
+
+# Vérification des variables Cloudinary
+CLOUDINARY_CLOUD_NAME = os.environ.get('CLOUDINARY_CLOUD_NAME')
+CLOUDINARY_API_KEY = os.environ.get('CLOUDINARY_API_KEY')
+CLOUDINARY_API_SECRET = os.environ.get('CLOUDINARY_API_SECRET')
+
+# Configuration conditionnelle de Cloudinary
+USE_CLOUDINARY = all([CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET])
+
+# Variable globale accessible dans les modèles
+USE_CLOUDINARY = USE_CLOUDINARY
+
+if USE_CLOUDINARY:
+    import cloudinary
+    import cloudinary.uploader
+    import cloudinary.api
+    
+    cloudinary.config(
+        cloud_name=CLOUDINARY_CLOUD_NAME,
+        api_key=CLOUDINARY_API_KEY,
+        api_secret=CLOUDINARY_API_SECRET,
+        secure=True
+    )
+    
+    CLOUDINARY_STORAGE = {
+        'CLOUD_NAME': CLOUDINARY_CLOUD_NAME,
+        'API_KEY': CLOUDINARY_API_KEY,
+        'API_SECRET': CLOUDINARY_API_SECRET,
+    }
+    DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+    print("Cloudinary configuré avec succès")
+else:
+    DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+    print("ATTENTION: Cloudinary non configuré. Utilisation du stockage local.")
 
 # ALLOWED_HOSTS
 if DEBUG:
@@ -39,11 +73,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'cloudinary_storage',
-    'cloudinary',
     'techEdgeApp',
-    
 ]
+
+# Ajouter Cloudinary seulement si configuré
+if USE_CLOUDINARY:
+    INSTALLED_APPS.insert(6, 'cloudinary_storage')
+    INSTALLED_APPS.insert(7, 'cloudinary')
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -92,6 +128,7 @@ AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
+
 import sys
 
 LOGGING = {
@@ -108,50 +145,17 @@ LOGGING = {
         "level": "DEBUG",
     },
 }
+
 # Internationalization
 LANGUAGE_CODE = 'fr-fr'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
-# Static & Media
-# Config Cloudinary
-# Configuration Cloudinary - AVANT les autres configurations
-CLOUDINARY_CLOUD_NAME = os.environ.get('CLOUDINARY_CLOUD_NAME')
-CLOUDINARY_API_KEY = os.environ.get('CLOUDINARY_API_KEY')
-CLOUDINARY_API_SECRET = os.environ.get('CLOUDINARY_API_SECRET')
-
-# Configurer Cloudinary explicitement
-if all([CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET]):
-    cloudinary.config(
-        cloud_name=CLOUDINARY_CLOUD_NAME,
-        api_key=CLOUDINARY_API_KEY,
-        api_secret=CLOUDINARY_API_SECRET,
-        secure=True
-    )
-    
-    CLOUDINARY_STORAGE = {
-        'CLOUD_NAME': CLOUDINARY_CLOUD_NAME,
-        'API_KEY': CLOUDINARY_API_KEY,
-        'API_SECRET': CLOUDINARY_API_SECRET,
-    }
-    DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-else:
-    print("ATTENTION: Variables Cloudinary manquantes. Utilisation du stockage local.")
-    DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
-    MEDIA_URL = '/media/'
-    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-
-# Configuration du storage pour les médias
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-
-# Static files (WhiteNoise pour les statics, Cloudinary pour les médias)
+# Static files
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
@@ -165,13 +169,169 @@ EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
-print("Cloudinary config:", os.getenv("CLOUDINARY_CLOUD_NAME"))
-if not DEBUG:
-    # Vérification des variables Cloudinary en production
-    required_env_vars = ['CLOUDINARY_CLOUD_NAME', 'CLOUDINARY_API_KEY', 'CLOUDINARY_API_SECRET']
-    for var in required_env_vars:
-        if not os.environ.get(var):
-            print(f"ATTENTION: Variable d'environnement manquante: {var}")
+# from pathlib import Path
+# import os
+# from dotenv import load_dotenv
+# import dj_database_url
+# import cloudinary
+# import cloudinary.uploader
+# import cloudinary.api
+# # Charger .env
+# load_dotenv()
+
+# BASE_DIR = Path(__file__).resolve().parent.parent
+
+# SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-change-me")
+# DEBUG = os.environ.get("DEBUG", "1") == "1"
+
+# # ALLOWED_HOSTS
+# if DEBUG:
+#     ALLOWED_HOSTS = ["*"]
+# else:
+#     ALLOWED_HOSTS = [h.strip() for h in os.environ.get("ALLOWED_HOSTS", "").split(",") if h]
+
+# CSRF_TRUSTED_ORIGINS = [f"https://{h}" for h in ALLOWED_HOSTS if h] + ["https://*.onrender.com"]
+
+# INSTALLED_APPS = [
+#     'django.contrib.admin',
+#     'django.contrib.auth',
+#     'django.contrib.contenttypes',
+#     'django.contrib.sessions',
+#     'django.contrib.messages',
+#     'django.contrib.staticfiles',
+#     'cloudinary_storage',
+#     'cloudinary',
+#     'techEdgeApp',
+    
+# ]
+
+# MIDDLEWARE = [
+#     'django.middleware.security.SecurityMiddleware',
+#     'whitenoise.middleware.WhiteNoiseMiddleware',
+#     'django.contrib.sessions.middleware.SessionMiddleware',
+#     'django.middleware.common.CommonMiddleware',
+#     'django.middleware.csrf.CsrfViewMiddleware',
+#     'django.contrib.auth.middleware.AuthenticationMiddleware',
+#     'django.contrib.messages.middleware.MessageMiddleware',
+#     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+# ]
+
+# ROOT_URLCONF = 'techEdgeWebSite.urls'
+
+# TEMPLATES = [
+#     {
+#         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+#         'DIRS': [os.path.join(BASE_DIR, 'techEdgeApp/templates')],
+#         'APP_DIRS': True,
+#         'OPTIONS': {
+#             'context_processors': [
+#                 'django.template.context_processors.debug',
+#                 'django.template.context_processors.request',
+#                 'django.contrib.auth.context_processors.auth',
+#                 'django.contrib.messages.context_processors.messages',
+#             ],
+#         },
+#     },
+# ]
+
+# WSGI_APPLICATION = 'techEdgeWebSite.wsgi.application'
+
+# # Database
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default=os.environ.get('DATABASE_URL', f"sqlite:///{BASE_DIR / 'db.sqlite3'}"),
+#         conn_max_age=600,
+#         ssl_require=not DEBUG
+#     )
+# }
+
+# # Password validation
+# AUTH_PASSWORD_VALIDATORS = [
+#     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+#     {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+#     {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+#     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
+# ]
+# import sys
+
+# LOGGING = {
+#     "version": 1,
+#     "disable_existing_loggers": False,
+#     "handlers": {
+#         "console": {
+#             "class": "logging.StreamHandler",
+#             "stream": sys.stdout,
+#         },
+#     },
+#     "root": {
+#         "handlers": ["console"],
+#         "level": "DEBUG",
+#     },
+# }
+# # Internationalization
+# LANGUAGE_CODE = 'fr-fr'
+# TIME_ZONE = 'UTC'
+# USE_I18N = True
+# USE_TZ = True
+
+# # Static & Media
+# # Config Cloudinary
+# # Configuration Cloudinary - AVANT les autres configurations
+# CLOUDINARY_CLOUD_NAME = os.environ.get('CLOUDINARY_CLOUD_NAME')
+# CLOUDINARY_API_KEY = os.environ.get('CLOUDINARY_API_KEY')
+# CLOUDINARY_API_SECRET = os.environ.get('CLOUDINARY_API_SECRET')
+
+# # Configurer Cloudinary explicitement
+# if all([CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET]):
+#     cloudinary.config(
+#         cloud_name=CLOUDINARY_CLOUD_NAME,
+#         api_key=CLOUDINARY_API_KEY,
+#         api_secret=CLOUDINARY_API_SECRET,
+#         secure=True
+#     )
+    
+#     CLOUDINARY_STORAGE = {
+#         'CLOUD_NAME': CLOUDINARY_CLOUD_NAME,
+#         'API_KEY': CLOUDINARY_API_KEY,
+#         'API_SECRET': CLOUDINARY_API_SECRET,
+#     }
+#     DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+# else:
+#     print("ATTENTION: Variables Cloudinary manquantes. Utilisation du stockage local.")
+#     DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
+#     MEDIA_URL = '/media/'
+#     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+# # Configuration du storage pour les médias
+# DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+# # Static files (WhiteNoise pour les statics, Cloudinary pour les médias)
+# STATIC_URL = '/static/'
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# # MEDIA_URL = '/media/'
+# # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# # Email
+# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+# EMAIL_HOST = os.environ.get("EMAIL_HOST")
+# EMAIL_PORT = int(os.environ.get("EMAIL_PORT", 587))
+# EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS", "False") == "True"
+# EMAIL_USE_SSL = os.environ.get("EMAIL_USE_SSL", "False") == "True"
+# EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+# EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+# DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+# print("Cloudinary config:", os.getenv("CLOUDINARY_CLOUD_NAME"))
+# if not DEBUG:
+#     # Vérification des variables Cloudinary en production
+#     required_env_vars = ['CLOUDINARY_CLOUD_NAME', 'CLOUDINARY_API_KEY', 'CLOUDINARY_API_SECRET']
+#     for var in required_env_vars:
+#         if not os.environ.get(var):
+#             print(f"ATTENTION: Variable d'environnement manquante: {var}")
 #config cloudinary
 
 
