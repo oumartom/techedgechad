638471ae3a3ad064b6ad40696f002c0d18a9b501
oumartom <85463550+oumartom@users.noreply.github.com>
2026-07-29 11:22:07 +0100

    Fix DJANGO_SETTINGS_MODULE environment variable

--- create_superuser.py
+++ create_superuser.py
@@ -1,7 +1,7 @@
 import os
 import django
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "techEdgeWebsite.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "techEdgeWebSite.settings")
 django.setup()
 
 from django.contrib.auth import get_user_model
